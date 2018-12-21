349459f8b12155184acb92e130ff8af13413e955
Chekistchek <chekistchek@gmail.com>
2018-12-21 19:54:54 +0300

    Added levels

--- SFMLT/src/Game.cpp
+++ SFMLT/src/Game.cpp
@@ -3,20 +3,30 @@
 #include <iostream> // test
 #include <algorithm>
 #include <memory>
+#include <string>
 #include "Game.h"
 #include "Snake.h"
 #include "Fruit.h"
+#include "Wall.h"
 
 namespace Bezhok {
 	using std::vector;
 	using std::ios_base;
 	using std::make_shared;
+	using std::ifstream;
+	using std::string;
+	using std::to_string;
+	using std::getline;
 
 	Game::Game()
 	{
+		// add smart pointers
 		m_data = make_shared<Game_Data>();
+		m_data->wall = make_shared<Wall>(m_data);
 		m_data->snake = make_shared<Snake>(m_data);
 		m_data->fruit = make_shared<Fruit>(m_data);
+
+		//m_data->objects.push_back(make_shared<Wall>());
 
 		m_data->window.create(sf::VideoMode(WIDTH, HEIGTH), "Snake");
 	
@@ -100,55 +110,96 @@ namespace Bezhok {
 		}
 	}
 
+	void Game::add2stats(int points)
+	{
+		// shift array
+		for (int i = 0; i < sizeof(m_stats) / sizeof(int); ++i) {
+			if (points > m_stats[i]) {
+				int temp = m_stats[i];
+
+				m_stats[i] = m_data->snake->m_points;
+
+				int prev = temp;
+				int curr;
+				for (int j = i + 1; j < sizeof(m_stats) / sizeof(int); ++j) {
+					curr = m_stats[j];
+					m_stats[j] = prev;
+					prev = curr;
+				}
+				break;
+			}
+		}
+		//
+	}
+
+	void Game::init_map(const string& fname)
+	{
+		vector<Wall::block> temp;
+
+		// read level's map
+		ifstream file(fname.c_str());
+		if (file.is_open()) {
+			string line;
+			for (int y = 0; y < Game::BLOCKS_COUNT_Y && getline(file, line); ++y) {
+				for (int i = 0, x = 0; x < Game::BLOCKS_COUNT_X && i < line.size(); ++x, ++i) {
+					if (line[i] == '0') {
+						temp.push_back(Wall::block{ x,y });
+					}
+					else if (line[i] == '\t') {
+						x += 3;
+					}
+				}
+			}
+			file.close();
+		}
+		else {
+			//error
+		}
+		m_data->wall->init(temp);
+	}
+
 	void Game::run()
 	{
+		string name = "levels/level2.txt"; // for example
+		init_map(name);
+		m_pause = true;
+
 		while (m_data->window.isOpen())
 		{
 			handle();
 
+			// delay todo
+			Sleep(100);
+
+			// logic starts
+			bool life = true;
 			if (!m_pause) {
-				// delay todo
-				Sleep(100);
+				life = m_data->snake->update();
+			}
 
-				// logic starts
-				bool life = m_data->snake->update();
+			// new frame start
+			m_data->window.clear(sf::Color::White);
+			m_data->snake->draw();
+			m_data->fruit->draw();
+			m_data->wall->draw();
 
-				// new frame start
-				m_data->window.clear(sf::Color::White);
-				m_data->snake->draw();
-				m_data->fruit->draw();
-				m_data->window.display();
+			if (m_pause) {
+			// menu
+			}
+			m_data->window.display();
+			
+			if (!life) {
+				add2stats(m_data->snake->m_points);
+				m_data->snake->reset();
+				m_data->fruit->reset();
 
-				if (!life) {
-					// shift array
-					for (int i = 0; i < sizeof(m_stats) / sizeof(int);++i) {
-						if (m_data->snake->m_points > m_stats[i]) {
-							int temp = m_stats[i];
-
-							m_stats[i] = m_data->snake->m_points;
-
-							int prev = temp;
-							int curr;
-							for (int j = i+1; j < sizeof(m_stats) / sizeof(int); ++j) {
-								curr = m_stats[j];
-								m_stats[j] = prev;
-								prev = curr;
-							}
-							break;
-						}
-					}
-					//
-					m_data->snake->init();
-					m_data->fruit->init();
-
-					// todo stop, output stats
-					for (int& x : m_stats) {
-						std::cout << x << std::endl;
-					}
-					std::cout << "\n\n";
-
-					m_pause = true;
+				// output stats
+				for (int& x : m_stats) {
+					std::cout << x << std::endl;
 				}
+				std::cout << "\n\n";
+
+				m_pause = true;
 			}
 		}
 	}
--- SFMLT/src/Game.h
+++ SFMLT/src/Game.h
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <memory>
+#include <string>
 
 namespace Bezhok {
 	using std::fstream;
@@ -11,15 +12,22 @@ namespace Bezhok {
 
 	class Snake;
 	class Fruit;
+	class GameObject;
+	class Wall;
 
 	typedef shared_ptr<Snake> snake_ref;
 	typedef shared_ptr<Fruit> fruit_ref;
+	typedef shared_ptr<Wall> wall_ref;
+
+	typedef shared_ptr<GameObject> game_object_ref;
 
 	struct Game_Data
 	{
 		sf::RenderWindow window;
 		snake_ref snake;
 		fruit_ref fruit;
+		wall_ref wall;
+		//vector<game_object_ref> objects;
 	};
 
 	typedef shared_ptr<Game_Data> game_data_ref;
@@ -50,5 +58,11 @@ namespace Bezhok {
 
 		/* handle events */
 		void handle();
+
+		/* shift stats array */
+		void add2stats(int points);
+
+		/* display new map */
+		void init_map(const std::string& fname);
 	};
 }
